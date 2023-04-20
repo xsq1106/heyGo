f833c5c0fdc6540454cd420996badbfe6130be8a
xsq1106 <1041309095@qq.com>
2023-04-20 18:33:05 +0800

    搜索功能

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,6 +1,33 @@
 <template>
 	<view>
-		cate
+		<my-search></my-search>
+		<view>
+		    <view class="scroll-view-container">
+		      <!-- 左侧的滚动视图区域 -->
+		      <scroll-view class="left-scroll-view" scroll-y :style="{height: wh + 'px'}">
+				<block v-for="(item, index) in cateList" :key="index">
+					<view @click="handleChange(item, index)" :class="['left-scroll-view-item', index === active ? 'active' : '']">{{item.cat_name}}</view>
+				</block>
+		      </scroll-view>
+		      <!-- 右侧的滚动视图区域 -->
+		      <scroll-view class="right-scroll-view" scroll-y :style="{height: wh + 'px'}" :scroll-top="scrollTop">
+		        <view class="cate-lv2" v-for="(item2, i2) in cateLevel2" :key="i2">
+		          <view class="cate-lv2-title">/ {{item2.cat_name}} /</view>
+				  <!-- 动态渲染三级分类的列表数据 -->
+				      <view class="cate-lv3-list">
+				        <!-- 三级分类 Item 项 -->
+				        <view class="cate-lv3-item" v-for="(item3, i3) in item2.children" :key="i3" @click="gotoGoodsList(item3)">
+				          <!-- 图片 -->
+				          <!-- <image :src="item3.cat_icon"></image> -->
+				          <!-- 文本 -->
+				          <text>{{item3.cat_name}}</text>
+				        </view>
+				      </view>
+
+		        </view>
+		      </scroll-view>
+		    </view>
+		  </view>
 	</view>
 </template>
 
@@ -8,12 +35,108 @@
 	export default {
 		data() {
 			return {
-				
+				active: 0,
+				wh: 0, // 窗口的可用宽度 = 屏幕高度- navigatorBar高度 - tabBargaodu
+				cateList: [],
+				cateLevel2: [],
+				scrollTop: 0,
 			};
-		}
+		},
+		onLoad() {
+			// 获取当前系统的信息
+			const sysInfo = uni.getSystemInfoSync()
+			// 为wh窗口可用高度动态赋值  -50减去搜索栏的高度
+			this.wh = sysInfo.windowHeight - 50
+			
+			this.getCateList()
+		},
+		methods: {
+			async getCateList() {
+				const { data: res } = await uni.$http.get('/api/public/v1/categories')
+				console.log(res, 'res')
+				if (res.meta.status !== 200) return uni.$showMsg
+				uni.$showMsg('数据请求成功')
+				this.cateList = res.message
+				// 为二级分类赋值
+				  this.cateLevel2 = res.message[0].children
+			},
+			handleChange(item, index) {
+				this.active = index
+				console.log(this.active, 'active')
+				this.cateLevel2 = this.cateList[index].children
+				console.log(this.scrollTop, 'scroot')
+				this.scrollTop = this.scrollTop ? 0 : 1
+			},
+				
+			gotoGoodsList(item) {
+				uni.navigateTo({
+					url: '/subpkg/goods_list/goods_list?cid' + item.cat_id
+				})
+			}
+		},
 	}
 </script>
 
 <style lang="scss">
+.scroll-view-container {
+  display: flex;
 
+  .left-scroll-view {
+    width: 120px;
+
+    .left-scroll-view-item {
+      line-height: 60px;
+      background-color: #f7f7f7;
+      text-align: center;
+      font-size: 12px;
+
+      // 激活项的样式
+      &.active {
+        background-color: #ffffff;
+        position: relative;
+
+        // 渲染激活项左侧的红色指示边线
+        &::before {
+          content: ' ';
+          display: block;
+          width: 3px;
+          height: 30px;
+          background-color: #c00000;
+          position: absolute;
+          left: 0;
+          top: 50%;
+          transform: translateY(-50%);
+        }
+      }
+    }
+  }
+}
+
+.cate-lv2-title {
+  font-size: 12px;
+  font-weight: bold;
+  text-align: center;
+  padding: 15px 0;
+}
+.cate-lv3-list {
+  display: flex;
+  flex-wrap: wrap;
+
+  .cate-lv3-item {
+    width: 33.33%;
+    margin-bottom: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    image {
+      width: 60px;
+      height: 60px;
+    }
+
+    text {
+      font-size: 12px;
+    }
+  }
+}
 </style>
---
+++ subpkg/search/search.vue
@@ -0,0 +1,151 @@
+<template>
+	<view>
+		<view class="search-box">
+			<uni-search-bar @input="input" :radius="100" cancelButton="none" placeholder="请输入搜搜内容"></uni-search-bar>
+		</view>
+		<!-- 搜索建议列表 -->
+		<view class="sugg-list" v-if="searchResults.length !== 0">
+		  <view class="sugg-item" v-for="(item, i) in searchResults" :key="i" @click="gotoDetail(item.goods_id)">
+		    <view class="goods-name">{{item.goods_name}}</view>
+		    <uni-icons type="arrowright" size="16"></uni-icons>
+		  </view>
+		</view>
+		<!-- 搜索历史 -->
+		<view class="history-box" v-else>
+		  <!-- 标题区域 -->
+		  <view class="history-title">
+		    <text>搜索历史</text>
+		    <uni-icons type="trash" size="17" @click="cleanHistory"></uni-icons>
+		  </view>
+		  <!-- 列表区域 -->
+		  <view class="history-list">
+		    <uni-tag :text="item" v-for="(item, i) in historyList" :key="i" @click="gotoGoodsList(item)"></uni-tag>
+		  </view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				timer: null,
+				searchValue: '',
+				searchResults: [],
+				historyList: ['a', 'app', 'apple']
+			};
+		},
+		onLoad() {
+			this.historyList = JSON.parse(uni.getStorageSync('searchHis'))
+		},
+		methods: {
+			input(e) {
+			    // e.value 是最新的搜索内容
+			    console.log(e)
+				// 做防抖处理
+				clearTimeout(this.timer)
+				this.timer = setTimeout(() => {
+					this.searchValue = e
+					this.getSearchList()
+				}, 500)
+			},
+			  // 根据搜索关键词，搜索商品建议列表
+			async getSearchList() {
+			    // 判断关键词是否为空
+			    if (this.searchValue === '') {
+			      this.searchResults = []
+			      return
+			    }
+			    // 发起请求，获取搜索建议列表
+			    const { data: res } = await uni.$http.get('/api/public/v1/goods/qsearch', { query: this.kw })
+			    if (res.meta.status !== 200) return uni.$showMsg()
+			    this.searchResults = res.message
+				this.saveSearchHistory()
+			  },
+			gotoDetail(goods_id) {
+				uni.navigateTo({
+				  // 指定详情页面的 URL 地址，并传递 goods_id 参数
+				  url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods_id
+				})
+			},
+			saveSearchHistory() {
+				let arr = [this.searchValue, ...this.historyList]
+				this.historyList = [...new Set(arr)]
+				
+				// 将记录存储在本地
+				uni.setStorageSync('searchHis', JSON.stringify(this.historyList))
+			},
+			cleanHistory() {
+				this.historyList = []
+				uni.setStorageSync('searchHis', '[]')
+			},
+			gotoGoodsList(item) {
+				uni.navigateTo({
+					url: '/subpkg/goods_list/goods_list?query='+item
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+.uni-searchbar {
+  /* #ifndef APP-NVUE */
+  display: flex;
+  /* #endif */
+  flex-direction: row;
+  position: relative;
+  padding: 16rpx;
+  /* 将默认的 #FFFFFF 改为 #C00000 */
+  background-color: #c00000;
+}
+.search-box {
+  position: sticky;
+  top: 0;
+  z-index: 999;
+}
+.sugg-list {
+  padding: 0 5px;
+
+  .sugg-item {
+    font-size: 12px;
+    padding: 13px 0;
+    border-bottom: 1px solid #efefef;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .goods-name {
+      // 文字不允许换行（单行文本）
+      white-space: nowrap;
+      // 溢出部分隐藏
+      overflow: hidden;
+      // 文本溢出后，使用 ... 代替
+      text-overflow: ellipsis;
+      margin-right: 3px;
+    }
+  }
+}
+.history-box {
+  padding: 0 5px;
+
+  .history-title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 40px;
+    font-size: 13px;
+    border-bottom: 1px solid #efefef;
+  }
+
+  .history-list {
+    display: flex;
+    flex-wrap: wrap;
+
+    .uni-tag {
+      margin-top: 5px;
+      margin-right: 5px;
+    }
+  }
+}
+</style>
